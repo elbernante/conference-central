7cafe6811b3d65335c0e8dbf911b24c1e0857f95
Peter James Bernante <elbernante@Peters-MacBook-Pro.local>
2015-09-14 12:40:41 -0400

    Add app id and client id

--- static/js/app.js
+++ static/js/app.js
@@ -86,7 +86,7 @@ app.constant('HTTP_ERRORS', {
  */
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
-        CLIENT_ID: 'web-client-id',
+        CLIENT_ID: '757224007118-0lblpo8abqeantp8mvckmabupik9edk4.apps.googleusercontent.com',
         SCOPES: 'email profile',
         signedIn: false
     }
