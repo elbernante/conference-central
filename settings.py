4e0ccbe520e63030e66233ad57681937b86cf81c
Peter James Bernante <elbernante@gmail.com>
2015-09-18 22:58:38 -0400

    Add android and ios client IDs

--- settings.py
+++ settings.py
@@ -13,6 +13,6 @@ created/forked from conference.py by wesc on 2014 may 24
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
 WEB_CLIENT_ID = '757224007118-0lblpo8abqeantp8mvckmabupik9edk4.apps.googleusercontent.com'
-ANDROID_CLIENT_ID = 'replace with Android client ID'
-IOS_CLIENT_ID = 'replace with iOS client ID'
+ANDROID_CLIENT_ID = '757224007118-dpqfa375ra8rgbpslig7beh4jb6qd03s.apps.googleusercontent.com'
+IOS_CLIENT_ID = '757224007118-nfgr65ic7dpiv5inbvta8a2b4j2h7d09.apps.googleusercontent.com'
 ANDROID_AUDIENCE = WEB_CLIENT_ID
